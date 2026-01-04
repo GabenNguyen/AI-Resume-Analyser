eb3d07e199976be94f8392d6334fb163c1eb3158
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2026-01-04 19:25:04 +1030

    Add floating blobs animated background

--- ai-resume-analyser/app/page.tsx
+++ ai-resume-analyser/app/page.tsx
@@ -1,24 +1,54 @@
+/* eslint-disable react-hooks/purity */
+"use client";
+
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
+import { motion } from "framer-motion";
 
 export default function HomePage() {
   return (
-    <>
-      <main className="flex flex-col">
+    <main className="relative min-h-screen overflow-hidden">
+      <div className="fixed inset-0 z-0 pointer-events-none">
+        {/* gradient */}
+        <motion.div
+          suppressHydrationWarning
+          animate={{ rotate: 360 }}
+          transition={{ repeat: Infinity, duration: 60, ease: "linear" }}
+          className="absolute inset-0 bg-linear-to-r from-blue-200 via-purple-200 to-pink-200 
+                     opacity-20 dark:from-gray-700 dark:via-gray-900 dark:to-gray-800 
+                     blur-3xl"
+        />
+
+        {/* floating blobs */}
+        {[...Array(8)].map((_, index) => (
+          <motion.div
+            suppressHydrationWarning
+            key={index}
+            animate={{ y: [0, 25, 0], x: [0, 15, 0] }}
+            transition={{
+              repeat: Infinity,
+              ease: "easeInOut",
+              duration: 5 + index,
+              delay: index,
+            }}
+            className="absolute w-24 h-24 rounded-full bg-blue-300/20 dark:bg-purple-700/20"
+            style={{
+              top: `${Math.random() * 100}%`,
+              left: `${Math.random() * 100}%`,
+            }}
+          />
+        ))}
+      </div>
+
+      {/* main contents */}
+      <div className="relative z-10 flex flex-col">
         <Hero />
-
-        <section className="py-10">
-          <Features />
-        </section>
-
-        <section className="py-10">
-          <HowItWorks />
-        </section>
-
+        <Features />
+        <HowItWorks />
         <Footer />
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
